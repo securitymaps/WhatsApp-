3cd54034d92358245ed2607f0e5f020c08eb5b65
securitymaps <emersonrestrepi@gmail.com>
2025-07-31 08:54:57 -0500

    node server.js

--- node server.js
+++ node server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 
 app.use(express.json());
 app.use(express.static("public")); // Carpeta para servir el HTML
