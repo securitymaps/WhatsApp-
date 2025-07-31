e6e119753ab783d0db449ea87e02c63d2b82ba7f
securitymaps <emersonrestrepi@gmail.com>
2025-07-31 08:45:01 -0500

    index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT || 3000;
 
 // Servir archivos estáticos desde la carpeta 'public'
 app.use(express.static(path.join(__dirname, 'public')));
