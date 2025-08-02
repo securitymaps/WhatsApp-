e414587964af970f6e7cfe2483b016346fadcfea
securitymaps <emersonrestrepi@gmail.com>
2025-08-01 23:10:03 -0500

    server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const botRoutes = require('./routes/bot');
 
 dotenv.config();
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 
 app.use(bodyParser.json());
 app.use('/webhook', botRoutes);
