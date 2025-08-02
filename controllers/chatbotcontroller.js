38599b523711066c0b6bfe19541bba4b8c638675
securitymaps <emersonrestrepi@gmail.com>
2025-08-01 22:26:30 -0500

    controllers/chatbotcontroller.js

---
+++ controllers/chatbotcontroller.js
@@ -0,0 +1,8 @@
+// controllers/chatbotController.js
+
+const handleMessage = (message) => {
+  console.log("📩 Mensaje recibido:", message);
+  // Aquí puedes poner la lógica para responder al mensaje
+};
+
+module.exports = { handleMessage };
