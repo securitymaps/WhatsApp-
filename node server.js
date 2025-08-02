a7163228fd9defc59370965c4da9d5ff8b704cb4
securitymaps <emersonrestrepi@gmail.com>
2025-08-01 22:08:46 -0500

    node server.js

---
+++ node server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+const botRoutes = require('./routes/bot');
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 10000;
+
+app.use(bodyParser.json());
+app.use('/webhook', botRoutes);
+
+app.get('/', (req, res) => {
+  res.send('VirtualBot de Zijin está funcionando');
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor ejecutándose en http://localhost:${PORT}`);
+});
